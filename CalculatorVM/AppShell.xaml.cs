0ce8a5d1ee255ea2bf09d43f1d3e8200f1118ed3
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-03-21 10:37:56 +0100

    Add project files.

---
+++ CalculatorVM/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorVM
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CalculatorVM/ViewModels/MainPageVM.cs
@@ -0,0 +1,56 @@
+﻿using _18_CRUD_Personas_UWP_UI.ViewModels.Utilidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorVM.ViewModels
+{
+    internal class MainPageVM : INotifyPropertyChanged
+    {
+        #region Attributes
+        private float num1;
+        private float num2;
+        private string selectedOp;
+        private float res;
+        private string displayText;
+        private DelegateCommand<string> selectOp;
+        private DelegateCommand<string> calculateNums;
+        private DelegateCommand<string> stringifyToDisplay;
+        #endregion
+        #region Properties
+        public string DisplayText { get; }
+        public DelegateCommand SelectOp { get; }
+        public DelegateCommand CalculateNums { get; }
+        public DelegateCommand StringifyToDisplay { get; }
+        #endregion
+        #region Constructor
+        public MainPageVM()
+        {
+            selectOp = new DelegateCommand<string>(selectOp_execute);
+            calculateNums = new DelegateCommand<string>(calculateNums_execute);
+            stringifyToDisplay = new DelegateCommand<string>(stringifyToDisplay_execute);
+        }
+        #endregion
+        #region Commands
+        private void selectOp_execute(string symbol)
+        {
+        }
+        private void calculateNums_execute(string num)
+        {
+        }
+        private void stringifyToDisplay_execute(string num)
+        { 
+        }
+        #endregion
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+        #region ViewModel
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        #endregion
+    }
+}
